5c55513b0a1971ac181715926fbddd2412f3be25
Gerson <gjimenezg9@miumg.edu.gt>
2026-02-27 20:58:20 -0600

    queue data structure

---
+++ queue/src/main/java/umg/edu/gt/data_structure/queue/manual/Node.java
@@ -0,0 +1,29 @@
+package umg.edu.gt.data_structure.queue.manual;
+
+
+public class Node<T> {
+     private T value; 
+    private Node<T> next;
+
+    Node(T value) {
+        this.value = value; 
+    }
+
+    public T getValue() {
+    	return value;
+    }
+    
+    public Node<T> getNext() {
+    	return next; 
+    	
+    }
+    
+    public void setNext(Node<T> next){
+    	this.next = next; 
+    }
+}
+
+
+
+
+
